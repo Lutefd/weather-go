7b42812a5a68d6d6270d29ab5b755d3ddbc22aae
Luis Dourado <lfaddourado@gmail.com>
2022-12-27 18:30:33 -0300

    feat: add structs

--- main.go
+++ main.go
@@ -1,1 +1,12 @@
-package weather_go
+package main
+
+type apiConfigData struct {
+	OpenWeatherMapKey string `json:"OpenWeatherMapApiKey"`
+}
+
+type weatherData struct {
+	Name string `json:"name"`
+	Main struct {
+		Celsius float64 `json:"temp"`
+	} `json:"main"`
+}
