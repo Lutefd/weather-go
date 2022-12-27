4f4bfd489b1ad6e64200442c7f3144cc6b21fdae
Luis Dourado <lfaddourado@gmail.com>
2022-12-27 18:50:44 -0300

    feat: add api configuration

--- main.go
+++ main.go
@@ -32,7 +32,27 @@ func loadApiConfig(filename string) (apiConfigData, error) {
 	}
 	return config, nil
 }
+func hello(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello!"))
+}
+func query(city string) (weatherData, error) {
+	apiConfig, err := loadApiConfig(".apiConfig")
+	if err!=nil{
+		return weatherData{}, err
+	}
+	resp, err := http.Get("http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apiConfig.OpenWeatherMapKey)
+	if err != nil {
+		return weatherData{}, err
+	}
+	defer resp.Body.Close()
 
+	var d weatherData
+
+	if err := json.NewDecoder(resp.Body).Decode(&d); err != nil {
+		return weatherData{}, err
+	}
+	return d, nil
+}
 func main() {
 	http.HandleFunc("/hello", hello)
 
@@ -49,3 +69,4 @@ func main() {
 
 	http.ListenAndServe(":8080", nil)
 }
+
